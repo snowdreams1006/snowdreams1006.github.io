6a13cc7a4c271002c8405ce03932f7e7f2151d4f
snowdreams1006 <snowdreams1006@163.com>
2019-06-04 16:59:34 +0800

    copy

--- gitbook/gitbook-plugin-mygitalk/mygitalk.js
+++ gitbook/gitbook-plugin-mygitalk/mygitalk.js
@@ -1,24 +1,20 @@
-require(["gitbook"], function(gitbook) {
+require([
+  "gitbook"
+], function(gitbook, $) {
+  var mygitalk = {};
+
   gitbook.events.bind("start", function(e, config) {
-    // gitalk 默认配置
-    const mygitalk = config.mygitalk;
-    
-    if (mygitalk) {
-      // 初始化 gitalk
-      mygitalk.id = window.location.pathname;
+    mygitalk = config.mygitalk || {};
 
-      const gitalk = new Gitalk(mygitalk);
-      gitalk.render("gitalk-container");
-
-      // 添加刷新按钮
-      gitbook.toolbar.createButton({
-        icon: "fa fa-refresh",
-        label: "mygitalk",
-        position: "right",
-        onClick: function() {
-          window.location.reload(true);
-        }
-      });
-    }
+    initMygitalk();
   });
+
+  gitbook.events.bind("page.change", initMygitalk);
+
+  function initMygitalk() {
+    mygitalk["id"] = window.location.pathname;
+
+    const gitalk = new Gitalk(mygitalk);
+    gitalk.render("gitalk-container");
+  }
 });
