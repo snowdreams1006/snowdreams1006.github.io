6c79b455b8fe9ca57e3290dcca87966f13f53078
snowdreams1006 <snowdreams1006@163.com>
2024-11-07 15:33:47 +0800

    auto pull and push

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,19 @@
 #!/bin/bash
 
-# copy to blog
-cp -rf ../snowdreams1006.github.io/assets/picgo/* assets/picgo
-
 # status latest 
 git status
-# pull latest
+
+# pull latest 
 git pull
+
 # add commits
 git add .
+
 # commit 
-git commit -m "upload changes"
+git commit -m "auto pull and push"
+
 # push to github and others
 git push
-# status latest 
-git status
-
-# generate docs 
-gitbook build
-
-# push to blog.snowdreams1006.cn
-scp -r _book/* ali:~/nginx/html/blog
-
-# copy to snowdreams1006.github.io
-cp -rf _book/* ../snowdreams1006.github.io
-
-# cd snowdreams1006.github.io
-cd ../snowdreams1006.github.io
 
 # status latest 
 git status
-# pull latest
-git pull
-# add commits
-git add .
-# commit 
-git commit -m "deploy website"
-# push to github and others
-git push
-# status latest 
-git status
-
-# cd blog
-cd ../blog
-
