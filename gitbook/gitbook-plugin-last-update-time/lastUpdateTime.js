8588accbd1a49ab41aeca0a7659de1dd8bcaabec
snowdreams1006 <snowdreams1006@163.com>
2025-01-28 23:20:48 +0800

    deploy website

--- gitbook/gitbook-plugin-last-update-time/lastUpdateTime.js
+++ gitbook/gitbook-plugin-last-update-time/lastUpdateTime.js
@@ -1,7 +1,15 @@
 require(['gitbook', 'jQuery'], function(gitbook, $) {
+    var getMonday = (date) => {
+      const currentDate = new Date(date);
+      const dayOfWeek = currentDate.getDay();
+      const difference = currentDate.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1);
+      currentDate.setDate(difference);
+      return currentDate;
+    }
+
     var init = function () {
         var title = $('title').text();
-        title = title + " " + new Date()
+        title = title + " " + getMonday(new Date()).toDateString()
         $('title').text(title);
     };
 
