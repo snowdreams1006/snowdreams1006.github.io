e925ac5684fcf473acfc197d632f8f1755ec3a8e
snowdreams1006 <snowdreams1006@163.com>
2026-03-05 12:36:06 +0800

    deploy website

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# copy to blog
-cp -rf ../snowdreams1006.github.io/assets/picgo/* assets/picgo
+# 检查第一个参数是否为空
+if [ -z "$1" ]; then
+    # copy to blog
+    cp -rf ../snowdreams1006.github.io/assets/picgo/* assets/picgo
+fi
 
 # status latest 
 git status
@@ -16,31 +19,38 @@ git push
 # status latest 
 git status
 
-# generate docs 
-gitbook build
+# 检查第一个参数是否为空
+if [ -z "$1" ]; then
+    # generate docs 
+    gitbook build
 
-# push to blog.snowdreams1006.cn
-scp -r _book/* ali:~/nginx/html/blog
+    # push to blog.snowdreams1006.cn
+    scp -r _book/* al:/usr/share/nginx/html/blog
 
-# copy to snowdreams1006.github.io
-cp -rf _book/* ../snowdreams1006.github.io
+    # copy to snowdreams1006.github.io
+    cp -rf _book/* ../snowdreams1006.github.io
 
-# cd snowdreams1006.github.io
-cd ../snowdreams1006.github.io
+    # cd snowdreams1006.github.io
+    cd ../snowdreams1006.github.io
 
-# status latest 
-git status
-# pull latest
-git pull
-# add commits
-git add .
-# commit 
-git commit -m "deploy website"
-# push to github and others
-git push
-# status latest 
-git status
+    # 删除全部md文件
+    find . -type f -name "*.md" -delete
 
-# cd blog
-cd ../blog
+    # status latest 
+    git status
+    # pull latest
+    git pull
+    # add commits
+    git add .
+    # commit 
+    git commit -m "deploy website"
+    # push to github and others
+    git push
+    # status latest 
+    git status
+
+    # cd blog
+    cd ../blog
+fi
+
 
